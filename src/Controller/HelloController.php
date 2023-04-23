d6283641040e341451812ef4b10bfbec73d38b4d
Ben <benjamin.baroche@free.fr>
2023-04-23 22:03:02 +0200

    page 3

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ControllerHello
 {
     /**
-     * @Route("/hello/{prenom?world}", name="hello")
+     * @Route("/hello/{prenom?world}", name="hello", methods={"GET", "POST"}, host="localhost", schemes={"https", "http"})
      */
 
     public function hello($prenom)
