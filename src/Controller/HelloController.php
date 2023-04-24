f188cefebdb0872516fa2f76508c89a71b4a903d
Ben <benjamin.baroche@free.fr>
2023-04-24 20:39:25 +0200

    maj controllerhello

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ControllerHello
 {
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
     /**
      * @Route("/hello/{prenom?World}", name="hello", methods={"GET", "POST"}, host="localhost", schemes={"https", "http"})
      */
 
     public function hello($prenom)
     {
+        $this->logger->info("mon msg de log");
         return new Response("Hello $prenom");
     }
 }
