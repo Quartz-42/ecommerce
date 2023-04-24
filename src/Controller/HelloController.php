b05ad45e3ad4b437652c22ec2241e59b3b88009d
Ben <benjamin.baroche@free.fr>
2023-04-24 20:59:41 +0200

    calculator

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,25 +2,27 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ControllerHello
 {
-    // protected $logger;
+    protected $calculator;
 
-    // public function __construct(LoggerInterface $logger)
-    // {
-    //     $this->logger = $logger;
-    // }
+    public function __construct(Calculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
     /**
      * @Route("/hello/{prenom?World}", name="hello", methods={"GET", "POST"}, host="localhost", schemes={"https", "http"})
      */
 
     public function hello($prenom)
     {
-        // $this->logger->info("mon msg de log");
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
         return new Response("Hello $prenom");
     }
 }
