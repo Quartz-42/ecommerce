c9b8e0a8c310f9b392f3e673ae429e52e941338a
Quartz-42 <benjamin.baroche@free.fr>
2023-04-28 11:34:34 +0200

    test

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,7 +6,7 @@ use App\Taxes\Calculator;
 use Cocur\Slugify\Slugify;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Twig\Environment;
 
 class ControllerHello
 {
@@ -20,8 +20,10 @@ class ControllerHello
      * @Route("/hello/{prenom?World}", name="hello", methods={"GET", "POST"}, host="localhost", schemes={"https", "http"})
      */
 
-    public function hello($prenom, Slugify $slug)
+    public function hello($prenom, Slugify $slug, Environment $twig)
     {
+        dump($twig);
+
         dump($slug->slugify("Hello World"));
         $tva = $this->calculator->calcul(100);
         dd($tva);
