76c5db67f589443bf73de744b90f1b7f5cb046fb
Quartz-42 <benjamin.baroche@free.fr>
2023-09-04 14:25:28 +0200

    Set data fixtures for Product entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,19 +6,28 @@ use Faker\Factory;
 use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager): void
     {
-
         $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
 
         for ($p = 0; $p < 100; $p++) {
             $product = new Product();
-            $product->setName($faker->sentence)
-                ->setPrice(mt_rand(100, 200))
-                ->setSlug($faker->slug());
+            $product->setName($faker->productName)
+                ->setPrice($faker->price(4000, 20000))
+                ->setSlug(strtolower($this->slugger->slug($product->getName())));
             $manager->persist($product);
         }
 
