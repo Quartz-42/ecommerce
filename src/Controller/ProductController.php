b2eb9313730120183d6121a13ee30b8aa3d22a87
Quartz-42 <benjamin.baroche@free.fr>
2023-09-05 14:10:23 +0200

    Create category and show templates

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/{slug}', name: 'product_category')]
+
+    public function category($slug, CategoryRepository $categoryRepository): Response
+    {
+
+        $category = $categoryRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        if (!$category) {
+            throw $this->createNotFoundException("La catégorie demandée n'existe pas");
+        }
+
+        return $this->render('product/category.html.twig', [
+            'slug' => $slug,
+            'category' => $category,
+        ]);
+    }
+
+    #[Route('/{category_slug}/{slug}', name: 'product_show')]
+
+    public function show($slug, ProductRepository $productRepository)
+    {
+        $product = $productRepository->findOneBy(
+            [
+                'slug' => $slug
+            ]
+        );
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit demandé n'existe pas");
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
