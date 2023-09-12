549b21a8ffc2c1c084de66b3979cd930bbb3ade8
Quartz-42 <benjamin.baroche@free.fr>
2023-09-12 20:40:16 +0200

    Enhance Category modification form

--- src/Form/Type/CategoryType.php
+++ src/Form/Type/CategoryType.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategoryType extends AbstractType
@@ -13,7 +14,7 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class)
             ->add('save', SubmitType::class);
     }
 
