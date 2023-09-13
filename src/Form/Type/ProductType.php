0116c11765be783885cb7c7d76786c15ed65aa04
Quartz-42 <benjamin.baroche@free.fr>
2023-09-13 11:45:16 +0200

    Enhance edit form for product

--- src/Form/Type/ProductType.php
+++ src/Form/Type/ProductType.php
@@ -2,21 +2,21 @@
 
 namespace App\Form\Type;
 
-use App\Entity\Product;
 use App\Entity\Category;
-use Doctrine\ORM\QueryBuilder;
+use App\Entity\Product;
 use App\Repository\CategoryRepository;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
 {
